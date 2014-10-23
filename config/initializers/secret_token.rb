a2c70b937fe137b48fa52942d059e20a49212092
mariastenquist <stenquistm@gmail.com>
2014-10-23 02:08:28 +0000

    First commit and README update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloccit::Application.config.secret_key_base = '2b06da601e35372624977a7ccc1bd533254192b93f36992988e1d0e1db60f06325e4eb1e9a2ebd31f7197590f676afd0a9759074a946d1df0b9e5caf656f7788'
